38004125a828669bf42915202c65b6bf8fcc61fe
antonio <antoniogeorgetoader@yahoo.ro>
2025-07-06 08:26:28 +0300

    token expiration

--- server/functions/createTokens.js
+++ server/functions/createTokens.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 const createAccessToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_ACCESS_SECRET, { expiresIn: '5m' })
+    return jwt.sign({ id }, process.env.JWT_ACCESS_SECRET, { expiresIn: `${process.env.JWT_ACCESS_TIME}m` })
 }
 
 const createRefreshToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '15m' })
+    return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, { expiresIn: `${process.env.JWT_REFRESH_TIME}m` })
 }
 
 module.exports = { createAccessToken, createRefreshToken }
